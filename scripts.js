9ed4b01f1d41b78190f0afd06545960988a88514
Batzuga <jere.ahvenus@gmail.com>
2024-09-04 11:18:07 +0300

    owning state filter

--- scripts.js
+++ scripts.js
@@ -11,6 +11,8 @@ else
 
 
 let doc;
+let srch;
+
 xmlhttp.onload = function() {
     var xmlDoc = new DOMParser().parseFromString(xmlhttp.responseText,'text/xml');
     doc = xmlDoc;
@@ -72,7 +74,51 @@ function CreateList(xmlDoc)
     document.getElementById("bookparent").innerHTML = full;
     document.getElementById("found").innerHTML = "Steelbooks Found: " + itemsFound;
 }
+function FilterList()
+{
+    let full = "";
+    var books = doc.getElementsByTagName("steelbook");
+    var itemsFound = 0;
+    var o = document.getElementById("owned").checked;
+    var no = document.getElementById("notowned").checked;
+    var w = document.getElementById("wishlist").checked;
+    for(i = 0; i < books.length; i++)
+    {
+        var title = books[i].getElementsByTagName("title")[0].innerHTML;
+        if(title.length == 0 || title == "") continue;
 
+        var owned = books[i].getElementsByTagName("status")[0].innerHTML;
+        if((owned == "Owned" && o) || (owned == "Not Owned" && no) || (owned == "Wishlist" && w))
+        {
+            var year = books[i].getElementsByTagName("release")[0].innerHTML;
+            var alias = books[i].getElementsByTagName("alias")[0].innerHTML;
+            if(alias == "") alias = "-";
+            var format = books[i].getElementsByTagName("format")[0].innerHTML;
+            
+            var img = books[i].getElementsByTagName("img")[0].innerHTML;
+           
+            if(title.length == 0 || title == "") continue;
+            itemsFound++;
+            let part = `
+            <div class="sb-preview">
+                <p class="sb-tag" title="${title}">${title}</p>
+                <div class="sb-tag half" title="Release Year: ${year}">${year}</div>
+                <div class="sb-tag half right">${alias}</div>
+                <div class="sb-imageview">
+                    <img class="sb-image" src="images/steelbooks/${img}"/>
+                </div>
+                <div class="sb-tag half">${format}</div>
+                <div class="sb-tag half right">${owned}</div>
+            </div>
+            `;
+            full += part;
+        }
+      
+    }
+    
+    document.getElementById("bookparent").innerHTML = full;
+    document.getElementById("found").innerHTML = "Steelbooks Found: " + itemsFound;
+}
 function SendRequest()
 {
     var title;
